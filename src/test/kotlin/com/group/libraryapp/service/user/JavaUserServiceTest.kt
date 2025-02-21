42d57f4d20bc1cccaec30ceee98c23d25ac16814
changha <chjeong@aift.kr>
2025-02-22 01:36:28 +0900

    13강. User, UserLoanHistory 도메인 Kotlin으로 변경

--- src/test/kotlin/com/group/libraryapp/service/user/JavaUserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/JavaUserServiceTest.kt
@@ -69,7 +69,7 @@ class UserServiceTest @Autowired constructor(
 
         // given
         val savedUser = userRepository.save(User("A", 20))
-        val request = UserUpdateRequest(savedUser.id, "B")
+        val request = UserUpdateRequest(savedUser.id!!, "B")
 
         // when
         userService.updateUserName(request)
